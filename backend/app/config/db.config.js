5489025ba1bb155183d7453e02fabe29362f41c9
philippepecoraro <philippe.pecoraro@bbox.fr>
2021-06-15 12:22:51 +0200

    Clear DB

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,16 +1,32 @@
 require('dotenv').config();
-module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: process.env.PASSWORD,
-    DB: "groupomania",
-    dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
 
-};
-
+if (process.env.NODE_ENV === 'development') {
+    module.exports = {
+        HOST: "localhost",
+        USER: "root",
+        PASSWORD: process.env.DEV_PASSWORD,
+        DB: "groupomania",
+        dialect: "mysql",
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        },
+    };
+}
+else {
+    module.exports = {
+        HOST: "eu-cdbr-west-01.cleardb.com",
+        USER: "ba2afdc92fb2f4",
+        PASSWORD: process.env.PROD_PASSWORD,
+        DB: "heroku_7fdaf929b34f1da",
+        dialect: "mysql",
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        },
+    };
+}
