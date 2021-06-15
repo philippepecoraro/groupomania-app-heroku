944acdf4c421327091234e1243bc17e69ca39e8a
philippepecoraro <philippe.pecoraro@bbox.fr>
2021-06-15 17:24:15 +0200

    modif Url production

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -9,6 +9,5 @@ export default axios.create({
 });
 
 console.log("NODE_ENV:" + process.env.NODE_ENV);
-console.log("BASE_URL:" + process.env.BASE_URL);
 console.log("VUE_APP_ENV_URL:" + process.env.VUE_APP_ENV_URL);
-console.log("VUE_APP_BASE:" + process.env.VUE_APP_BASE);
+
