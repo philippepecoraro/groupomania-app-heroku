284b132556b2d11a211b772f4e96d9ac185b2e90
philippepecoraro <philippe.pecoraro@bbox.fr>
2021-06-18 17:51:40 +0200

    ENV

--- backend/app/config/dbProd.config.js
+++ backend/app/config/dbProd.config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 module.exports = {
     HOST: "eu-cdbr-west-01.cleardb.com",
     USER: "ba2afdc92fb2f4",
-    PASSWORD: process.env.PROD_PASSWORD,
+    PASSWORD: "90f5d244262457e",
     DB: "heroku_7fdaf929b34f1da",
     dialect: "mysql",
     pool: {
