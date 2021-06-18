c3e9b4cf531320ab4a060a2737c28cf7f3bff4db
philippepecoraro <philippe.pecoraro@bbox.fr>
2021-06-18 18:03:38 +0200

    PROD_PASSWORD sur Héroku

--- backend/app/config/dbProd.config.js
+++ backend/app/config/dbProd.config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 module.exports = {
     HOST: "eu-cdbr-west-01.cleardb.com",
     USER: "ba2afdc92fb2f4",
-    PASSWORD: "90f5d244262457e",
+    PASSWORD: process.env.PROD_PASSWORD,
     DB: "heroku_7fdaf929b34f1da",
     dialect: "mysql",
     pool: {
